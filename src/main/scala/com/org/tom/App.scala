af6b00a07e12aefafa78e2313bc2d2de0031b46b
ram1805 <ramdharshnan@gmail.com>
2019-10-17 08:39:58 +0530

    Project structure

---
+++ src/main/scala/com/org/tom/App.scala
@@ -0,0 +1,9 @@
+package com.org.tom
+
+/**
+ * Hello world!
+ *
+ */
+object App extends App {
+  println( "Hello World!" )
+}
---
+++ src/test/scala/com/org/tom/Application.scala
@@ -0,0 +1,17 @@
+package com.org.tom
+
+import org.junit._
+import Assert._
+
+@Test
+class Application {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
